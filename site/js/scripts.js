9f405348de297c1e0c54301397c9b2d234275e6e
Ian Morgan <ian.j.morgan@gmail.com>
2023-04-10 19:37:35 +0200

    minor updates to content and styles

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -8,6 +8,31 @@ function collapse(btn,content) {
   document.getElementById(btn).style.display = "inline";
 }
 
+function copyInputClipboard(element) {
+  // Get the text field
+  var copyText = document.getElementById(element);
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
+
+function copyTextToClipboard(theText) {
+  // Get the text field
+
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(theText);
+
+
+}
+
 //function showmenu() {
 //  document.getElementById("nav").style.display = "none";
 //  //document.getElementById(btn).style.display = "inline";
