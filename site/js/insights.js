e8e6bca584f023324271cd1527e2ffa74c14870f
Ian Morgan <ian.j.morgan@gmail.com>
2023-06-07 14:25:42 +0100

    move insights tracking to its own js file

---
+++ site/js/insights.js
@@ -0,0 +1,8 @@
+insights.init('WuFrwhmdgsPVi_pq');
+insights.trackPages( {
+  parameters: {
+    locale: insights.parameters.locale(),
+    screenSize: insights.parameters.screenType(),
+    referrer: insights.parameters.referrer()
+   }
+});
