7d7ce10ee3ee0b551c9c8e3bbdb40fac21722e6e
Ian Morgan <ian.j.morgan@gmail.com>
2024-03-12 21:20:16 +0000

    added openday QR codes

---
+++ create-qr-code.py
@@ -0,0 +1,15 @@
+
+# see https://realpython.com/python-generate-qr-code/
+
+
+# basic_qrcode.py
+
+import segno
+
+payload = "https://www.hpyc.org.uk/the-club/openday"
+output = "openday"
+
+qrcode = segno.make_qr(payload)
+qrcode.save(output + ".png", scale=5,)
+qrcode.save(output + "-red.png",  dark="darkred", scale=5,)
+qrcode.save(output + "-blue.png",  dark="darkblue", scale=5,)
