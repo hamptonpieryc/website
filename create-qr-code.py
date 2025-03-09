baaa5d3d0fcc21e4f3a39f96922f3c47423fd2cf
Ian Morgan <ian.j.morgan@gmail.com>
2025-03-09 20:56:49 +0000

    re-enable open day pages - wrong year - again !

--- create-qr-code.py
+++ create-qr-code.py
@@ -6,10 +6,13 @@
 
 import segno
 
-payload = "https://www.hpyc.org.uk/openday"
-output = "openday"
+payload = "https://sailing.myclub.run/landing/rya2025"
+output = "RYA"
 
 qrcode = segno.make_qr(payload)
 qrcode.save(output + ".png", scale=5,)
 qrcode.save(output + "-red.png",  dark="darkred", scale=5,)
 qrcode.save(output + "-blue.png",  dark="darkblue", scale=5,)
+qrcode.save(output + "-purple.png",  dark="#9724DB", light="#aaaaaa", scale=5,)
+qrcode.save(output + "-alternatee.png",  dark="#3C24DB", light="#aaaaaa", scale=5,)
+
