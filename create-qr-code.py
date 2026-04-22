0ac3b363ec713856511d898b16015421b9b3a886
Ian Morgan <ian.j.morgan@gmail.com>
2026-04-22 23:25:45 +0100

    adding background image to open day

--- create-qr-code.py
+++ create-qr-code.py
@@ -6,13 +6,18 @@
 
 import segno
 
-payload = "https://sailing.myclub.run/landing/rya2025"
-output = "RYA"
+#payload = "https://sailing.myclub.run"
+#output = "myclub"
+
+payload = "https://hpyc.org.uk/openday"
+output = "opendat"
 
 qrcode = segno.make_qr(payload)
 qrcode.save(output + ".png", scale=5,)
 qrcode.save(output + "-red.png",  dark="darkred", scale=5,)
 qrcode.save(output + "-blue.png",  dark="darkblue", scale=5,)
 qrcode.save(output + "-purple.png",  dark="#9724DB", light="#aaaaaa", scale=5,)
-qrcode.save(output + "-alternatee.png",  dark="#3C24DB", light="#aaaaaa", scale=5,)
+qrcode.save(output + "-black.png",  dark="#000000", scale=5,)
+
+#qrcode.save(output + "-alternatee.png",  dark="#3C24DB", light="#aaaaaa", scale=5,)
 
